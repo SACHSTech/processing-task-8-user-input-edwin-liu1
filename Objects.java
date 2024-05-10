31aa3d9b520eafd9e5b07411df1104730ec361db
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-05-10 13:01:38 -0400

    oops

---
+++ Objects.java
@@ -0,0 +1,19 @@
+class Objects{
+    class OLine{
+        float x, y, x2, y2;
+        DLine (float X, float Y, float X2, float Y2){
+            x = X;
+            y = Y;
+            x2 = X2;
+            y2 = Y2;
+        }
+
+    }
+    class OCircle{
+        float x, y;
+        OCiecle (float X, float Y;){
+            x = X;
+            y = Y;
+        }
+    }
+}
