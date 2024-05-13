83da2c69647f4c8a5499ab8d07e6492dfa9972e8
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-05-13 14:16:19 -0400

    i wanna add an undo feature

--- Sketch.java
+++ Sketch.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 import processing.event.MouseEvent;
 
@@ -7,8 +9,8 @@ public class Sketch extends PApplet {
   boolean shift = false;
   int size = 10;
 
-  OLine[] lines = new OLine[0];
-  OCircle[] circles = new OCircle[0];
+  ArrayList<OLine> lines = new ArrayList<OLine>();
+  ArrayList<OCircle> circles = new ArrayList<OCircle>();
   
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -22,22 +24,24 @@ public class Sketch extends PApplet {
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
-  public void setup() {}
+  public void setup() {stroke(255);}
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
     background(backgroundR, backgroundG, backgroundB);
-    if (mousePressed && !shift){append(circles, new OCircle(mouseX, mouseY, size * 2) );}
-    stroke(0); fill(255);
+    if (mousePressed && !keyPressed == (keyCode == SHIFT) ){circles.add(new OCircle(mouseX, mouseY, size * 2) );}
+    noFill();
     for (OLine line : lines){
       strokeWeight(line.getWidth() );
-      line(line.getX(), line.getY(), line.getX2(), line.getY2());
+      line(line.getX(), line.getY(), line.getX2(), line.getY2() );
     }
     for (OCircle circle : circles){
+      strokeWeight(2);
       circle(circle.getX(), circle.getY(), circle.getRadius() );
     }
+    fill(255);
     circle(mouseX, mouseY, size);
   }
   
@@ -45,19 +49,18 @@ public class Sketch extends PApplet {
     if ('r' == key){backgroundR = 255;}
     if ('g' == key){backgroundG = 255;}
     if ('b' == key){backgroundB = 255;}
-    if (SHIFT == keyCode){shift = true;}
+    if (BACKSPACE == keyCode){lines.clear(); circles.clear();}
   }
 
   public void keyReleased(){
     if ('r' == key){backgroundR = 0;}
     if ('g' == key){backgroundG = 0;}
     if ('b' == key){backgroundB = 0;}
-    if (SHIFT == keyCode){shift = false;}
   }
 
   public void mouseDragged(){
-    if (shift){
-      append(lines, new OLine(pmouseX, pmouseY, mouseX, mouseY, size) );
+    if (keyPressed == (keyCode == SHIFT) ){
+      lines.add(new OLine(pmouseX, pmouseY, mouseX, mouseY, size) );
     }
   }
 
